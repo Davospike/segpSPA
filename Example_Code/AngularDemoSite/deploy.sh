9916616e5493634a96dfa13b944964b916886e10
nwtay <nathan.w.taylor@outlook.com>
2021-04-04 16:27:56 +0100

    added space for new data points before implementation

--- Example_Code/AngularDemoSite/deploy.sh
+++ Example_Code/AngularDemoSite/deploy.sh
@@ -4,7 +4,6 @@ docker build .
 docker-compose up -d
 
 # setting up the db
-# - need to find a way to clear the DB 
 # insert data into the collections
 docker exec -i db sh -c 'mongoimport -u "your_username" -p "your_password" --authenticationDatabase "admin" -c newstopics -d fakeNewsDB --upsert --jsonArray' < blockData/newsTopicsOutput.json
 docker exec -i db sh -c 'mongoimport -u "your_username" -p "your_password" --authenticationDatabase "admin" -c quizquestions -d fakeNewsDB --upsert --jsonArray' < blockData/quizQuestionsOutput.json
