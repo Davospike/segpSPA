53515ec2c29ff3053eef975ccd9f91b9577767e8
Harry Smith <harrysmithhjs@gmail.com>
2021-04-07 17:12:03 +0100

    re add quiz app with landing page

---
+++ Example_Code/AngularQuizApp/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'stats', component: StatisticsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    WelcomePageComponent,
+    ToolbarComponent,
+    StatisticsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
