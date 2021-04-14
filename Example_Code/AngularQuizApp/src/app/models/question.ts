f5d3b9a4580d4a73c7ca1cede05329332cc75996
Harry Smith <harrysmithhjs@gmail.com>
2021-04-14 11:07:18 +0100

    Add article url body to question and links to url witht answers

--- Example_Code/AngularQuizApp/src/app/models/question.ts
+++ Example_Code/AngularQuizApp/src/app/models/question.ts
@@ -3,6 +3,8 @@ import { Option } from './option';
 export class Question {
     id: number;
     name: string;
+    articleBody: string;
+    articleUrl: string;
     questionTypeId: number;
     options: Option[];
     answered: boolean;
@@ -11,6 +13,8 @@ export class Question {
         data = data || {};
         this.id = data.id;
         this.name = data.headline;
+        this.articleBody = data.text_body;
+        this.articleUrl = data.correct_answer_url;
         this.questionTypeId = data.questionTypeId;
         this.options = [];
         data.options.forEach(o => {
