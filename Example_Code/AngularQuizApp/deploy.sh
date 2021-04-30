e09717cf0b109f058b93a1ad19907e48ec58a0a1
Jack Pike <jackpike18@gmail.com>
2021-04-30 11:15:20 +0100

    scripts

--- Example_Code/AngularQuizApp/deploy.sh
+++ Example_Code/AngularQuizApp/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#docker-compose build
-#docker-compose up -d
-
 docker-compose up --build -d
+
+username=$(grep "MONGO_USERNAME" .env | cut -d '=' -f2)
+password=$(grep "MONGO_PASSWORD" .env | cut -d '=' -f2)
 
 # setting up the db
 # insert data into the collections
-docker exec -i db sh -c 'mongoimport -u "your_username" -p "your_password" --authenticationDatabase "admin" -c options -d db --upsert --jsonArray' < blockData/optionsOutput.json
-docker exec -i db sh -c 'mongoimport -u "your_username" -p "your_password" --authenticationDatabase "admin" -c newstopics -d db --upsert --jsonArray' < blockData/newsTopicsOutput.json
-docker exec -i db sh -c 'mongoimport -u "your_username" -p "your_password" --authenticationDatabase "admin" -c quizquestions -d db --upsert --jsonArray' < blockData/quizQuestionsOutput.json
+docker exec -i db sh -c 'mongoimport -u $username -p $password --authenticationDatabase "admin" -c options -d db --upsert --jsonArray' < bloc>
+docker exec -i db sh -c 'mongoimport -u $username -p $password --authenticationDatabase "admin" -c newstopics -d db --upsert --jsonArray' < b>
+docker exec -i db sh -c 'mongoimport -u $username -p $password --authenticationDatabase "admin" -c quizquestions -d db --upsert --jsonArray' >
--- Example_Code/AngularQuizApp/src/app/topics/topics.component.spec.ts
+++ Example_Code/AngularQuizApp/src/app/topics/topics.component.spec.ts
@@ -1,4 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TopicsComponent } from './topics.component';
@@ -6,13 +7,20 @@ import { TopicsComponent } from './topics.component';
 describe('TopicsComponent', () => {
   let component: TopicsComponent;
   let fixture: ComponentFixture<TopicsComponent>;
+  let html: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule ],
       declarations: [ TopicsComponent ],
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+        fixture = TestBed.createComponent(TopicsComponent);
+        component = fixture.componentInstance;
+        html = fixture.debugElement;
+        fixture.detectChanges;
+    })
   }));
 
   beforeEach(() => {
@@ -24,4 +32,15 @@ describe('TopicsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should move to to Coronavirus quiz path', () => {
+    spyOn(component, null);
+    const CoronaLink = html.query(By.css('CoronaLink'));
+    CoronaLink.triggerEventHandler('Take the Coronavirus quiz', null);
+    tick();
+    fixture.detectChanges();
+    expect(component.ngOnInit).toHaveBeenCalled();
+  });
 });
+
+
