f5cc3d84ed5797925f7c1468516d1e782385ff2a
Jack Pike <jackpike18@gmail.com>
2021-04-30 09:14:22 +0100

    karma config

--- Example_Code/AngularQuizApp/src/app/app-routing.module.ts
+++ Example_Code/AngularQuizApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {BarChartComponent} from './bar-chart/bar-chart.component';
 import {WelcomePageComponent} from './welcome-page/welcome-page.component';
 import {QuizComponent} from './quiz/quiz.component';
 import {StatisticsComponent} from './statistics/statistics.component';
@@ -12,7 +11,6 @@ const routes: Routes = [
   { path: 'quiz/:id', component: QuizComponent },
   { path: 'topics', component: TopicsComponent },
   { path: 'about', component: StatisticsComponent },
-  { path: 'bar-chart', component: BarChartComponent},
   { path: '**', component: WelcomePageComponent }
 ];
 
